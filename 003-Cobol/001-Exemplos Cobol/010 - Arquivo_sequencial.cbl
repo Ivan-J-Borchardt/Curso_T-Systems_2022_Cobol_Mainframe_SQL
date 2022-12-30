f6c5edaf4ce213ebdf66b0d4cc3da7fb0d761c6e
Ivan Borchardt <ivan.borchardt.cobol@gmail.com>
2022-12-30 20:35:46 -0300

    Recuperacao do Exemplo 010

--- 003-Cobol/001-Exemplos Cobol/010 - Arquivo_sequencial.cbl
+++ 003-Cobol/001-Exemplos Cobol/010 - Arquivo_sequencial.cbl
@@ -2,11 +2,11 @@
 
       *>Divisão de identificação do programa
        identification division.
-       program-id. "balance_line".
+       program-id. "arquivo_sequencial".
        author. "Ivan J. Borchardt".
        installation. "PC".
-       date-written. 14/12/2022.
-       date-compiled. 14/12/2022.
+       date-written. 09/11/2022.
+       date-compiled. 09/11/2022.
 
 
 
@@ -19,20 +19,10 @@
        input-output section.
        file-control.
 
-           select arqAlunos assign to "arqAlunosDados.txt"
+           select arqAlunos assign to "arqAlunos.txt"
            organization is line sequential
            access mode is sequential
            file status is wk-fs-arqAlunos.
-
-           select arqNotas assign to "arqAlunosNotas.txt"
-           organization is line sequential
-           access mode is sequential
-           file status is wk-fs-arqNotas.
-
-           select arqBoletin assign to "arqBoletin.txt"
-           organization is line sequential
-           access mode is sequential
-           file status is wk-fs-arqBoletin.
 
 
 
@@ -44,44 +34,30 @@
       *>----Variaveis de arquivos
        file section.
        fd arqAlunos.
-       01  fd-aluno.
-           05  fd-aluno-matricula                    pic  x(10).
-           05  fd-aluno-nome                         pic  x(25).
-           05  fd-aluno-serie                        pic  9(02).
-           05  fd-aluno-cpf                          pic  x(12).
+      *>01  fd-aluno.
+      *>    05  fd-aluno-nome                        pic  x(10).
+      *>    05  fd-aluno-serie                       pic  9(02).
+      *>    05  fd-aluno-cpf                         pic  x(12).
+       copy aluno.
 
-       fd arqNotas.
-       01  fd-notas.
-           05 fd-notas-matricula                     pic  x(10).
-           05 fd-notas-disciplina                    pic  x(04).
-           05 fd-notas-nota occurs 15                pic  9(02)v99 comp-3.
-
-       fd arqBoletin.
-       01  fd-boletin.
-           05 fd-boletin-matricula                   pic  x(10).
-           05 fd-boletin-nome                        pic  x(25).
-           05 fd-boletin-serie                       pic  9(02).
-           05 fd-boletin-disciplina                  pic  x(04).
-           05 fd-boletin-nota occurs 15              pic  9(02)v99 comp-3.
-           05 fd-boletin-media                       pic  9(02)v99 comp-3.
 
       *>----Variaveis de trabalho
        working-storage section.
 
-      *>     File Status
-       77  wk-fs-arqAlunos                            pic x(02).
+       77 wk-fs-arqAlunos                          pic 9(02).
 
-       77  wk-fs-arqNotas                             pic x(02).
+       77 ws-sair                                  pic  x(01).
+          88  fechar-programa                      value "N" "n".
+          88  voltar-tela                          value "V" "v".
 
-       77  wk-fs-arqBoletin                           pic x(02).
+       77  ws-menu                                 pic  x(02).
 
-      *>     Estatisticas
-       77  wk-qtd-alunos-lidos                        pic 9(06) comp.
-       77  wk-qtd-notas-lidos                         pic 9(06) comp.
+       01  wk-aluno  occurs 1000.
+           05  wk-aluno-nome                        pic  x(10).
+           05  wk-aluno-serie                       pic  9(02).
+           05  wk-aluno-cpf                         pic  x(12).
 
-      *>     Mensagem de erro
-       77  wk-msn                                     pic x(60).
-
+       77  wk-ind                                   pic  9(04) value 0.
 
       *>----Variaveis para comunicação entre programas
        linkage section.
@@ -94,211 +70,146 @@
        procedure division.
 
 
+           perform exemplo-leitura.
+           perform exemplo-escrita.
+           perform processamento.
+           perform finaliza.
 
-           perform a-inicializa.
-           perform b-processa.
-           perform z-finaliza.
-
-
-      *>*****************************************************************
-      *>   Procedimento de Incializaçao
-      *>*****************************************************************
-       a-inicializa section.
-       a-inicializa-a.
-
-           open input arqAlunos
-           if wk-fs-arqAlunos not equal "00" then
-              string "Erro abertura Arquivo arqAlunos: " wk-fs-arqAlunos
-                      delimited by size
-                into wk-msn
-              end-string
-
-              perform z-finaliza-erro
+      *>------------------------------------------------------------------------
+      *>  Lendo o arquivo
+      *>------------------------------------------------------------------------
+       exemplo-leitura section.
+           open input  arqAlunos
+           if wk-fs-arqAlunos <> 0 then
+               display "Erro ao abrir Arquivo arqAlunos " wk-fs-arqAlunos
            end-if
 
-           open input arqNotas
-           if wk-fs-arqNotas not equal "00" then
-              string "Erro abertura Arquivo arqNotas: " wk-fs-arqNotas
-                      delimited by size
-                into wk-msn
-              end-string
 
-              perform z-finaliza-erro
-           end-if
+           perform until wk-fs-arqAlunos <> 0
+               read arqAlunos
+               if  wk-fs-arqAlunos <> 0
+               and wk-fs-arqAlunos <> 10 then
+                   display "Erro ao ler Arquivo arqAlunos " wk-fs-arqAlunos
+               else
+                   add  1              to    wk-ind
+                   move fd-aluno       to    wk-aluno(wk-ind)
+               end-if
+            end-perform
 
-           open output arqBoletin
-           if wk-fs-arqBoletin not equal "00" then
-              string "Erro abertura Arquivo arqBoletin: " wk-fs-arqBoletin
-                      delimited by size
-                into wk-msn
-              end-string
 
-              perform z-finaliza-erro
+
+
+
+           close arqAlunos
+           if wk-fs-arqAlunos <> 0 then
+               display "Erro ao fechar Arquivo arqAlunos " wk-fs-arqAlunos
            end-if
 
 
            .
-       a-inicializa-z.
+       exemplo-leitura-exit.
            exit.
 
-      *>*****************************************************************
-      *>   Procedimento Principal
-      *>*****************************************************************
-       b-processa section.
-       b-processa-a.
-
-           perform ba-ler-aluno
-           perform bb-ler-notas
-
-           perform until wk-fs-arqAlunos  equal "10"
-                     and wk-fs-arqNotas   equal "10"
-
-              evaluate true
-                 when fd-aluno-matricula equal fd-notas-matricula
-                    perform bc-gravar-boletin
-                    perform ba-ler-aluno
-                    perform bb-ler-notas
-
-                 when fd-aluno-matricula less than fd-notas-matricula
-                    perform ba-ler-aluno
-
-                 when fd-aluno-matricula greater than fd-notas-matricula
-                    perform bb-ler-notas
 
 
-              end-evaluate
+      *>------------------------------------------------------------------------
+      *>  escrevendo no arquivo
+      *>------------------------------------------------------------------------
+       exemplo-escrita section.
+
+      *>   open output  arqAlunos
+           open extend  arqAlunos
+           if wk-fs-arqAlunos <> 0 then
+               display "Erro ao abrir Arquivo arqAlunos " wk-fs-arqAlunos
+           end-if
+
+
+           move "Henrique"     to    fd-aluno-nome
+           move 7              to    fd-aluno-serie
+           move "000000000-25" to    fd-aluno-cpf
+
+           write fd-aluno
+           if  wk-fs-arqAlunos <> 0  then
+               display "Erro ao escrever Arquivo arqAlunos " wk-fs-arqAlunos
+           end-if
+
+
+           close arqAlunos
+           if wk-fs-arqAlunos <> 0 then
+               display "Erro ao fechar Arquivo arqAlunos " wk-fs-arqAlunos
+           end-if
+
+
+           .
+       exemplo-escrita-exit.
+           exit.
+
+
+
+      *>------------------------------------------------------------------------
+      *>  Processamento principal
+      *>------------------------------------------------------------------------
+       processamento section.
+
+           perform until fechar-programa
+               move space to ws-sair
+               display "'Ca'dastrar"
+               display "'Co'nsultar"
+
+               accept ws-menu
+
+               evaluate ws-menu
+                   when = "Ca"
+                       perform cadastra-temp
+
+                   when = "Co"
+                       perform consultar
+
+                   when other
+                       display "opcao invalida"
+               end-evaluate
+
 
            end-perform
 
+
            .
-       b-processa-z.
+       processamento-exit.
            exit.
 
-
-      *>*****************************************************************
-      *>   Leitura do arquivo arqAluno
-      *>*****************************************************************
-       ba-ler-aluno section.
-       ba-ler-aluno-a.
-
-           read arqAlunos
-             at end
-                move "10"            to    wk-fs-arqAlunos
-                move high-values     to    fd-aluno-matricula
-             not at end
-                if   wk-fs-arqAlunos  not equal "00"
-                and  wk-fs-arqAlunos  not equal "10"  then
-                   move spaces       to    wk-msn
-                   string "Erro Leitura arquivo arqAlunos, File Status: "
-                           wk-fs-arqAlunos   delimited by size
-                     into wk-msn
-                   end-string
-                   perform  z-finaliza-erro
-                end-if
-
-                if wk-fs-arqAlunos equal "00" then
-                   add 1 to wk-qtd-alunos-lidos
-                end-if
-           end-read
+      *>------------------------------------------------------------------------
+      *>  Rotina de consulta - lê o arquivo
+      *>------------------------------------------------------------------------
+       consultar section.
 
            .
-       ba-ler-aluno-z.
-           exit.
-
-      *>*****************************************************************
-      *>   Leitura do arquivo arqNotas
-      *>*****************************************************************
-       bb-ler-notas section.
-       bb-ler-notas-a.
-
-           read arqNotas
-             at end
-                move "10"            to    wk-fs-arqNotas
-                move high-values     to    fd-notas-matricula
-             not at end
-                if   wk-fs-arqNotas  not equal "00"
-                and  wk-fs-arqNotas  not equal "10"  then
-                   move spaces       to    wk-msn
-                   string "Erro Leitura arquivo arqNotas, File Status: "
-                           wk-fs-arqNotas   delimited by size
-                     into wk-msn
-                   end-string
-                   perform  z-finaliza-erro
-                end-if
-
-                if wk-fs-arqNotas equal "00" then
-                   add 1 to wk-qtd-notas-lidos
-                end-if
-           end-read
-
-           .
-       bb-ler-notas-z.
+       consultar-exit.
            exit.
 
 
 
 
-      *>*****************************************************************
-      *>   Procedimento de Finaliazaçao com erro
-      *>*****************************************************************
-       z-finaliza-erro section.
-       z-finaliza-erro-a.
-           display erase
-           display wk-msn
 
-           stop run.
+      *>------------------------------------------------------------------------
+      *>  Rotina de cadastro  - escreve no arquivo
+      *>------------------------------------------------------------------------
+       cadastra-temp section.
+
            .
-       z-finaliza-erro-z.
+       cadastra-temp-exit.
            exit.
 
 
 
-      *>*****************************************************************
-      *>   Procedimento de Finaliazaçao Normal
-      *>*****************************************************************
-       z-finaliza section.
-       z-finaliza-a.
-           display erase
-           display "Finalizando Programa..."
+      *>------------------------------------------------------------------------
+      *>  Finalização
+      *>------------------------------------------------------------------------
+       finaliza section.
 
-           close arqAlunos
-           if wk-fs-arqAlunos not equal "00" then
-              string "Erro fechamento Arquivo arqAlunos: " wk-fs-arqAlunos
-                      delimited by size
-                into wk-msn
-              end-string
-
-              perform z-finaliza-erro
-           end-if
-
-           close arqNotas
-           if wk-fs-arqNotas not equal "00" then
-              string "Erro fechamento Arquivo arqNotas: " wk-fs-arqNotas
-                      delimited by size
-                into wk-msn
-              end-string
-
-              perform z-finaliza-erro
-           end-if
-
-           close arqBoletin
-           if wk-fs-arqBoletin not equal "00" then
-              string "Erro fechamento Arquivo arqBoletin: " wk-fs-arqBoletin
-                      delimited by size
-                into wk-msn
-              end-string
-
-              perform z-finaliza-erro
-           end-if
-
-
-           stop run.
+           Stop run
            .
-       z-finaliza-z.
+       finaliza-exit.
            exit.
-
-
-
 
 
 
