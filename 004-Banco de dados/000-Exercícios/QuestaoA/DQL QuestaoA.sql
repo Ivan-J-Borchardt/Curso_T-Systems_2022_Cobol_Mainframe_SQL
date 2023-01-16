428535ffaff2b906dd523a85ad95ef192df7010a
Ivan Borchardt <ivan.borchardt.cobol@gmail.com>
2023-01-16 18:01:18 -0300

    Correção Questao A

--- 004-Banco de dados/000-Exercícios/QuestaoA/DQL QuestaoA.sql
+++ 004-Banco de dados/000-Exercícios/QuestaoA/DQL QuestaoA.sql
@@ -1,4 +1,4 @@
---
+-- Forma simples de selecionar dados 
 select * from mae; 
 
 select cod, nome from mae; 
@@ -34,3 +34,53 @@ select *
   from mae 
  where cod > 2
    and not(data_nasc > '01/01/2000'); 
+
+-- order by 
+select * 
+  from cidade 
+ order by cidade; 
+
+  select * 
+  from cidade 
+ order by cidade  desc; 
+ 
+select * 
+  from  mae 
+  order by nome desc, data_nasc asc; 
+  
+-- Alias de coluna 
+ select cod as Codigo, 
+        nome as Mãe, 
+        data_nasc as NAscimento
+   from mae;
+
+-- Join 
+select mae.cod, 
+       mae.nome, 
+       endereco.cep, 
+       endereco.numero
+   from mae, 
+        endereco 
+  where mae.cod_end = endereco.cod; 
+
+--
+ select mae.cod, 
+       mae.nome, 
+       mae.data_nasc,
+       endereco.cep, 
+       endereco.numero, 
+       rua.rua, 
+       cidade.cidade,
+       estado.estado
+   from mae, 
+        endereco, 
+        rua, 
+        cidade, 
+        estado
+  where mae.cod_end = endereco.cod
+    and endereco.cod_rua = rua.cod
+    and endereco.cod_cidade = cidade.cod
+    and endereco.cod_estado = estado.cod
+    and data_nasc BETWEEN '1990-01-01' and '1999.12.31'
+    order by data_nasc;  
+  
--- 004-Banco de dados/000-Exercícios/QuestaoA/Exercício DQL.md
+++ 004-Banco de dados/000-Exercícios/QuestaoA/Exercício DQL.md
@@ -3,3 +3,43 @@
 3. Selecione o DDD e Número dos telefones cujo DDD é '047'; 
 4. Selecione as mães que nasceram na década de 90; 
 5. Selecione as mães cujos últimos dois digitos do CPF seja igual a 13.  
+
+-- 1 
+select * 
+  from mae
+where nome like '%Oliveira';
+
+-- 2 
+select * 
+  from cidade 
+ where cod not in(1, 3, 5);
+ 
+ -- 2 
+ select * 
+   from cidade 
+  where cod <> 1
+    and cod <> 3
+    and cod <> 5; 
+ 
+ -- 3
+ select ddd, 
+        numero
+  from mae_telefone
+ where ddd = '047'; 
+ 
+ select * from mae_telefone; 
+ 
+ -- 4 
+ select * 
+   from mae 
+ where data_nasc >= '1990-01-01'
+   and data_nasc <= '1999.12.31';
+   
+ select * 
+   from mae 
+ where data_nasc BETWEEN '1990-01-01' and '1999.12.31';
+ 
+ -- 5 
+ select * 
+   from mae
+  where cpf like '%13';  
